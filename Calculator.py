a332a772d0849b63be355437d728ec6d2310c26f
Jordan Day <jordanday444@gmail.com>
2014-08-06 16:12:07 -0500

    Initial Commit

---
+++ Calculator.py
@@ -0,0 +1,37 @@
+import sys
+from math import *
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+class Form(QDialog):
+
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+
+        self.browser = QTextBrowser()
+        self.lineEdit = QLineEdit("Type an expression and press Enter.")
+        self.lineEdit.selectAll()
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.browser)
+        layout.addWidget(self.lineEdit)
+        self.setLayout(layout)
+
+        self.lineEdit.setFocus()
+
+        #self.connect(self.lineEdit, SIGNAL("returnPressed()"), self.updateUi)
+        self.lineEdit.returnPressed.connect(self.updateUi)
+
+        self.setWindowTitle("Calculate")
+
+    def updateUi(self):
+        try:
+            text = self.lineEdit.text()
+            self.browser.append("%s = <b>%s</b>" % (text, eval(text)))
+        except:
+            self.browser.append("<font color=red>%s is invalid</font>" % text)
+
+app = QApplication(sys.argv)
+form = Form()
+form.show()
+form.exec_()
