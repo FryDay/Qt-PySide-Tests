0f03083856871e60b5ef8a457163179e1cb8773a
Jordan Day <jordanday444@gmail.com>
2014-08-08 10:36:34 -0500

    Finished Currency Converter.

--- Currency.py
+++ Currency.py
@@ -9,8 +9,10 @@ class Form(QDialog):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
 
-        date = self.getData()
+        date = self.getDate()
         rates = sorted(self.rates.keys())
+
+        self.setWindowTitle("Currency Converter")
 
         dateLabel = QLabel(date)
         self.fromComboBox = QComboBox()
@@ -31,9 +33,9 @@ class Form(QDialog):
         grid.addWidget(self.toLabel, 2, 1)
         self.setLayout(grid)
 
-        self.fromComboBox.currentIndexChanged.connect(partial(self.updateUi, self.fromComboBox.currentIndex()))
-        self.toComboBox.currentIndexChanged.connect(partial(self.updateUi, self.toComboBox.currentIndex()))
-        self.fromSpinBox.valueChanged.connect(partial(self.updateUi, self.fromSpinBox.value()))
+        self.fromComboBox.currentIndexChanged.connect(self.updateUi)
+        self.toComboBox.currentIndexChanged.connect(self.updateUi)
+        self.fromSpinBox.valueChanged.connect(self.updateUi)
 
     def updateUi(self):
         to = self.toComboBox.currentText()
@@ -51,9 +53,24 @@ class Form(QDialog):
             fh = urllib2.urlopen("http://www.bankofcanada.ca/stats/assets/csv/fx-seven-day.csv")
             for line in fh:
                 line = line.rstrip()
-                if not line or line.startswith("#", "Closing"):
+                if not line or line.startswith(("#", "Closing")):
                     continue
 
-                field = line.split(",")
+                fields = line.split(",")
+                if line.startswith("Date "):
+                    date = fields[-1]
+                else:
+                    try:
+                        value = float(fields[-1])
+                        self.rates[fields[0]] = value
+                    except ValueError:
+                        pass
 
-        except:
+            return "Exchange rates for " + date
+        except Exception, e:
+            return "Failed to download:\n%s" % e
+
+app = QApplication(sys.argv)
+form = Form()
+form.show()
+app.exec_()
